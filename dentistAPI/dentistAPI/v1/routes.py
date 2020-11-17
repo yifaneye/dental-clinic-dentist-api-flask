243b7aaf9ddd8504bec3dcaf831020ce8c6040e8
Yifan Ai <ayfallen@hotmail.com>
2020-11-18 03:09:24 +1100

    Edit to improve API schema

--- dentistAPI/dentistAPI/v1/routes.py
+++ dentistAPI/dentistAPI/v1/routes.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
+###
+### DO NOT CHANGE THIS FILE
+### 
+### The code is auto generated, your change will be overwritten by 
+### code generating.
+###
 from __future__ import absolute_import
 
 from .api.dentists import Dentists
 from .api.dentists_id import DentistsId
+
 
 routes = [
     dict(resource=Dentists, urls=['/dentists'], endpoint='dentists'),
